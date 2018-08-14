3c4d8f88cb16e261ad367301b644fb8cd27afe83
username <jimmy.woodard@comcast.net>
2018-08-14 03:18:44 -0600

    more verbosity

--- scanner.sh
+++ scanner.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 start_time="$(date +%s)"
-uptime="$(uptime)"
-freemem="$(free -g)"
-freedisk="$(df -h | grep '/'|sort -nr -k5)"
 docroots="$(cat /etc/httpd/conf.d/*.conf |grep DocumentRoot | grep -v '#'|awk '{print $2}'|sort |uniq)"
 hostname="$(hostname)"
-sendmailqueue="$(mailq | tail -n1 | awk '{print $3}')"
-postfixmailqueue="$(mailq | tail -n1 | awk '{print $5}')"
 webuser="unknown"
 arch="$(head -n1 /etc/issue)"
 maldethits="$(maldet -l | grep '\-\-report' | tail -n1 |awk '{print $NF}')"
@@ -18,40 +13,10 @@ elif [[ "$arch" == *"Ubuntu"* ]]; then
    webuser="www-data"
 fi
 
-# Hostname
-echo "Hostname: $hostname"
-echo
-
-# Uptime
-echo "Uptime:"
-echo "$uptime"
-echo
-
-# Free Memory
-echo "Memory Usage (Gigabytes):"
-echo "$freemem"
-echo
-
-# Disk Space
-echo "Disk Space:"
-echo "$freedisk"
-echo
-
-# Mail Queue
-if pgrep -x "master" > /dev/null;
-then
-    echo "Postfix Queue: $postfixmailqueue"
-elif pgrep -x "sendmail" > /dev/null;
-then
-    echo "Sendmail Queue: $sendmailqueue"
-else
-    echo "Unknown Mail System"
-fi
-echo
-echo
-
 
 # Clean up from last run
+echo "Step 1 of 9"
+echo "Cleaning up from last run"
 if [ ! -d "/opt/scripts/" ]; then
   mkdir -p /opt/scripts/
 fi
@@ -59,15 +24,21 @@ fi
 if [ -f "/opt/scripts/scan_results.txt" ]; then
   rm -rf /opt/scripts/scan_results.txt
 fi
+echo "Clean up complete."
+
+
 
 # Maldet scan
+echo "Step 2 of 9"
 echo "Scanning with maldet. This could take awhile."
 maldet -u
 freshclam
 maldet -a /
 echo "Maldet scan complete"
 
+
 # Maldet results from last scan
+echo "Step 3 of 9"
 echo "Getting Maldet results from last scan and adding to /opt/scripts/scan_results.txt"
 echo "During a routine scan of your server, $hostname, we detected one or more suspicious files indicating the presence of malware on your server. Most often these are a result of an out of date or unpatched CMS, or unpatched plugins or themes.
 
@@ -82,20 +53,21 @@ echo >> /opt/scripts/scan_results.txt
 find /usr/local/maldetect/sess/ -name session.hits."$maldethits" -exec cat | grep -v 'rfxn.ndb\|rfxn.hdb\|rfxn.yara\|hex.dat\|md5.dat\|\/home\/bmesh_admin' {} \; >> /opt/scripts/scan_results.txt
 echo >> /opt/scripts/scan_results.txt
 echo >> /opt/scripts/scan_results.txt
-echo "Complete"
+echo "Maldet Results Complete."
 echo
 
 
 
-#CMS Updates Listing Routine
-echo "Step 1 of 5"
+# Docroot enumeration
+echo "Step 4 of 9"
 echo "Enumerating docroots"
 echo "Docroots found in /etc/httpd/conf.d/*.conf:"
 echo "$docroots"
 echo "Docroot enumeration complete"
 echo
 
-echo "Step 2 of 5"
+
+echo "Step 5 of 9"
 echo "Scanning for oustanding Drupal/Wordpress updates. This can take awhile, please be patient."
 echo "Here is a list of outstanding Drupal/Wordpress updates. If a module/theme/plugin is listed as having an update available, you will need to apply these ASAP, even if the provided module/theme/plugin is not in use:" >> /opt/scripts/scan_results.txt
 for docroot in $docroots; do echo ; cd "$docroot" ; pwd ; wp core version  --allow-root 2>/dev/null ; wp plugin list --allow-root 2>/dev/null | grep -i 'available' ; wp theme list --allow-root 2>/dev/null | grep -i 'available' ; drush up --security-only -n 2>/dev/null | grep -i 'SECURITY UPDATE available' ; done >> /opt/scripts/scan_results.txt
@@ -110,8 +82,9 @@ echo
 echo "Additionally, we found the following suspicious files that may have not been detected by our malware scanning software. Please note that this secondary list is likely to contain false-positives, but should still be investigated:" >> /opt/scripts/scan_results.txt
 echo >> /opt/scripts/scan_results.txt
 
-#PHP files in /uploads/ or /files/
-echo "Step 3 of 5"
+
+# PHP files in /uploads/ or /files/
+echo "Step 6 of 9"
 echo "Searching for PHP files within /var/www/*/htdocs/wp-content/uploads/ and /var/www/*/htdocs/sites/default/files/ ."
 echo "PHP files within /var/www/*/htdocs/wp-content/uploads/ and /var/www/*/htdocs/sites/default/files/ ." >> /opt/scripts/scan_results.txt
 echo "These can be malicious and should be reviewed manually and removed if they are indeed non-legit files:" >> /opt/scripts/scan_results.txt
@@ -123,7 +96,7 @@ echo "PHP file scan complete"
 echo
 
 # Binaries within /var/www/ /var/tmp/ /var/lib/dav/ /tmp/ and /dev/shm/
-echo "Step 4 of 5"
+echo "Step 7 of 9"
 echo "Searching for Binary files within /dev/shm, /var/tmp, /var/lib/dav, and /var/www/ . This can take awhile, please be patient. " 
 echo "Binary files found within /dev/shm/, /var/tmp, /var/lib/dav, /tmp and /var/www/ . " >> /opt/scripts/scan_results.txt
 echo "These can be malicious and should be reviewed manually and removed if they are indeed non-legit files:" >> /opt/scripts/scan_results.txt
@@ -136,20 +109,20 @@ echo
 
 # Files owned apache:apache or www-data:www-data within /var/www/ /var/tmp/ /var/lib/dav/ /tmp/ /dev/shm/
 # Note: this portion will need filtering added as a pipe to 'grep -v' or blacklisting added to the find command. Until then, expect this to be verbose
-echo "Step 6 of 7"
+echo "Step 8 of 9"
 echo "Scanning for files owned $webuser:$webuser within /tmp, /var/tmp, /var/www and /dev/shm/. " 
 echo "Files owned apache:apache within /tmp, /var/tmp, /var/lib/dav, /var/www and /dev/shm:" >> /opt/scripts/scan_results.txt
 echo "These can be malicious and should be reviewed manually and removed if they are indeed non-legit files:" >> /opt/scripts/scan_results.txt
 find /tmp/ /var/tmp/ /dev/shm/ /var/lib/dav/ /var/www/ -type f -user $webuser -group $webuser | grep -v 'css$\|js$\|js.gz$\|css.gz$\|png$\|jpg$\|jpeg$\|pdf$\|gif$\|gz.info$\|doc$\|docx$\|cache\|twig\|gluster\|proc' >> /opt/scripts/scan_results.txt
 echo >> /opt/scripts/scan_results.txt
 echo >> /opt/scripts/scan_results.txt
-echo "Scan complete. Results are in /opt/scripts/scan_results.txt"
+echo "File scan complete."
 echo
 
 
 # Directories owned apache:apache or www-data:www-data within /var/www/ /var/tmp/ /var/lib/dav/ /tmp/ /dev/shm/
 # Note: this portion will need filtering added as a pipe to 'grep -v' or blacklisting added to the find command. Until then, expect this to be verbose
-echo "Step 7 of 7"
+echo "Step 9 of 9"
 echo "Scanning for directories owned $webuser:$webuser within /tmp, /var/tmp, /var/www and /dev/shm/. " 
 echo "Directories owned apache:apache within /tmp, /var/tmp, /var/lib/dav, /var/www and /dev/shm:" >> /opt/scripts/scan_results.txt
 echo "These can be malicious and should be reviewed manually and removed if they are indeed non-legit directories." >> /opt/scripts/scan_results.txt
@@ -157,7 +130,7 @@ echo "A large number of directories here could indicate the need for a recursive
 find /tmp/ /var/tmp/ /dev/shm/ /var/lib/dav/ /var/www/ -type d -user $webuser -group $webuser  >> /opt/scripts/scan_results.txt
 echo >> /opt/scripts/scan_results.txt
 echo >> /opt/scripts/scan_results.txt
-echo "Scan complete. Results are in /opt/scripts/scan_results.txt"
+echo "Directory scan complete."
 echo
 
 
